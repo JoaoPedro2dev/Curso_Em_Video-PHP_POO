0095ba648a775e8da51f15ad07c3e6e2cb3743fe
JoaoPedro2Dev <joaopdev1@gmail.com>
2025-05-02 12:36:17 -0300

    Aula Teórica 8 – Relacionamento de Agregação
    Aprendendo como fazer relacionamento do tipo Agregação entre classes, utilizando também um exemplo prático

--- Pratica/aulaPratica7/Luta.php
+++ Pratica/aulaPratica7/Luta.php
@@ -1,16 +1,93 @@
 <?php 
     class luta{
+        //Atributos
         private $desafiado;
         private $desafiante;
         private $rounds;
         private $aprovada;
 
-        public function marcarLuta(){
+        //Métodos
+        public function marcarLuta($l1, $l2){
+            if($l1->getCategoria() != $l2->getCategoria()){
+                return "Erro: Não pode ocorrer luta de categorias diferentes";
+            }
 
+            if($l1->getNome() == $l2->getNome()){
+                return "Erro: Um lutador não pode lutar com ele mesmo";
+            }
+
+            $this->setAprovada(true);
+            
+            $this->setDesafiado($l1);
+            $this->setDesafiante($l2);
+
+            echo "luta agendada";
         }
 
         public function lutar(){
-            
+            if(!$this->getAprovada()){
+                return "Erro: A luta não pode acontecer, não foi aprovada";
+            }
+
+            echo "A luta entre {$this->desafiado->getNome()} e {$this->desafiante->getNome()} começou";
+            echo "Desafiado <br> {$this->desafiado->apresentar()}";
+            echo "Desafiante <br> {$this->desafiante->apresentar()}<br>";
+            echo "<h1>Resultado</h1>";
+
+            $resultado = rand(0, 2);
+
+            switch ($resultado){
+                case 0:
+                    echo "A luta empatou";
+                    $this->desafiado->empatarLuta();
+                    $this->desafiante->empatarLuta();
+                    break;
+                case 1:
+                    echo "O lutador {$this->desafiado->getNome()} foi o vencedor";
+                    $this->desafiado->ganharLuta();
+                    $this->desafiante->perderLuta();
+                    break;
+                case 2:
+                    echo "O lutador {$this->desafiante->getNome()} foi o vencedor";
+                    $this->desafiante->ganharLuta();
+                    $this->desafiado->perderLuta();
+                    break;
+                default :
+                    echo "Erro: algo inesperado aconteceu na luta";
+            }
+        }  
+
+        //Métodos especiais
+        public function getDesafiado(){
+            return $this->desafiado;
+        }
+
+        public function setDesafiado($valor){
+            $this->desafiado = $valor;
+        }
+
+        public function getDesafiante(){
+            return $this->desafiante;
+        }
+
+        public function setDesafiante($valor){
+            $this->desafiante = $valor;
+        }
+
+        public function getRound(){
+            return $this->rounds;
+        }
+
+        public function setRound($valor){
+            $this->rounds = $valor;
+        }
+
+        public function getAprovada(){
+            return $this->aprovada;
+        }
+
+        public function setAprovada($valor){
+            $this->aprovada = $valor;
         }
     }
 ?>
--- Pratica/aulaPratica7/index.php
+++ Pratica/aulaPratica7/index.php
@@ -10,8 +10,11 @@
 <body>
     <?php 
             require_once"./Lutador.php";
+            require_once"./Luta.php";
 
             $descricao = [["Pretty Boy", "Americano", 32, 1.89, 78.8, 6, 2, 1], ["Putscript", "Brasileiro", 23, 1.82, 65.5, 5, 0, 0], ["Snapshadow", "Frances", 35, 1.75, 65, 10, 2, 2], ["Dead Code", "Europeu", 20, 1.90, 100, 8, 3, 1], ["", "Peruano", 30, 1.86, 103.8, 11, 6, 1], ["", "italiano", 38, 1.78, 78.6, 18, 9, 2]];
+
+            echo "<h1>Lutadores</h1>";
 
             for($i = 0; $i < count($descricao); $i++){
                 $lutador[$i] = new Lutador(...$descricao[$i]);
@@ -19,6 +22,15 @@
                 $lutador[$i]->status();
                 echo $lutador[$i]->getCategoria();
             }
+
+            echo "<h1>Luta</h1>";
+
+            $luta01 = new Luta;
+            echo $luta01->marcarLuta($lutador[1], $lutador[2])."<br><br>";
+
+            echo "<h1>A luta começou</h1>";
+
+            echo $luta01->lutar() . "<br><br>";
         ?>
 </body>
 
